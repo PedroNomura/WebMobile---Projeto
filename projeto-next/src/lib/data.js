0aa2e748a09ea44aee161aac19c92f6da7dcdfc0
Victor Vaglieri de Oliveira <127432508+Victor-Vaglieri@users.noreply.github.com>
2025-11-09 09:22:02 -0300

    adicionando as APIs

--- projeto-next/src/lib/data.js
+++ projeto-next/src/lib/data.js
@@ -1,9 +1,12 @@
-export const produtos = [
+// lib/data.js
+
+
+const staticProducts = [
     { 
         id: "samsung_galaxy", 
         nome: "Smartphone Samsung Galaxy", 
         preco: 999.99, 
-        imagem: "/img/samsung galaxy.png",
+        imagem: "https://res.cloudinary.com/dsyxtpek7/image/upload/v1762690662/samsung_galaxy_d9pmpr.png",
         descricao: "Smartphone com tela de 6.1 polegadas e câmera tripla",
         precoAntigo: "R$ 1.299,99",
         etiqueta: "Oferta"
@@ -11,41 +14,62 @@ export const produtos = [
     { 
         id: "camiseta_polo", 
         nome: "Camiseta Polo Masculina", 
-        preco: 89.99, 
-        imagem: "/img/polo.png",
-        descricao: "Camiseta polo de algodão 100% em diversas cores"
+        preco: 89.99,
+        imagem: "https://res.cloudinary.com/dsyxtpek7/image/upload/v1762690662/polo_fecfhr.png",
+        descricao: "Camiseta polo de algodão 100% em diversas cores",
+        precoAntigo: null, 
+        etiqueta: null
     },
     { 
         id: "notebook_lenovo", 
         nome: "Notebook Lenovo IdeaPad", 
         preco: 2299.99, 
-        imagem: "/img/lenovo.png",
+        imagem: "https://res.cloudinary.com/dsyxtpek7/image/upload/v1762690662/lenovo_lrxmde.png",
         descricao: "Notebook com processador Intel Core i5 e 8GB RAM",
+        precoAntigo: null,
         etiqueta: "Novo"
     },
     { 
         id: "kit_jardim", 
         nome: "Kit Jardim Completo", 
         preco: 79.99, 
-        imagem: "/img/kit.png",
-        descricao: "Kit com ferramentas básicas para jardinagem"
+        imagem: "https://res.cloudinary.com/dsyxtpek7/image/upload/v1762690662/kit_cyxwle.png",
+        descricao: "Kit com ferramentas básicas para jardinagem",
+        precoAntigo: null,
+        etiqueta: null
     },
     { 
         id: "tenis_nike", 
         nome: "Tênis Esportivo Nike", 
         preco: 299.99, 
-        imagem: "/img/nike.png",
-        descricao: "Tênis para corrida com tecnologia Air Max"
+        imagem: "https://res.cloudinary.com/dsyxtpek7/image/upload/v1762690663/nike_fsadbp.png",
+        descricao: "Tênis para corrida com tecnologia Air Max",
+        precoAntigo: null,
+        etiqueta: null
     },
     { 
         id: "kit_maquiagem", 
         nome: "Kit Maquiagem Completo", 
         preco: 129.99, 
-        imagem: "/img/maquiagem.png",
-        descricao: "Kit com batom, base, rímel e sombras variadas"
+        imagem: "https://res.cloudinary.com/dsyxtpek7/image/upload/v1762690663/maquiagem_qsrvzb.png",
+        descricao: "Kit com batom, base, rímel e sombras variadas",
+        precoAntigo: null,
+        etiqueta: null
     },
 ];
 
-export function getProductById(id) {
-    return produtos.find(p => p.id === id);
+
+
+export async function getAllProducts() {
+    return Promise.resolve(staticProducts);
+}
+
+export async function getProductById(id) {
+    const product = staticProducts.find(p => p.id === id);
+    
+    if (product) {
+        return Promise.resolve(product);
+    } else {
+        return Promise.resolve(null);
+    }
 }
