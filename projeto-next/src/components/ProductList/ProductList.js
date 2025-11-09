6c7ca30e57063dc526ba7bda36368d174eaf43e2
Victor Vaglieri de Oliveira <127432508+Victor-Vaglieri@users.noreply.github.com>
2025-11-09 09:22:47 -0300

    Add files via upload

--- projeto-next/src/components/ProductList/ProductList.js
+++ projeto-next/src/components/ProductList/ProductList.js
@@ -1,8 +1,7 @@
 import ProductCard from "../ProductCard/ProductCard";
-import { produtos } from "@/lib/data";
 import styles from "./ProductList.module.css"
 
-export default function ProductList() {
+export default function ProductList({ produtos }) {
     return (
         <section id="produtos" className={`${styles.secaoProdutos} destinos-nav`}>
             <h2>Produtos em Destaque</h2>
